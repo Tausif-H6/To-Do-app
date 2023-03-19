7e1b45096ba37c32807a70fed733e3af167184da
Tausif <tsftousif2@gmail.com>
2023-03-19 20:57:30 +0800

    Registration part done

--- base/views.py
+++ base/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView,FormView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -15,6 +18,26 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm #We are submitting the form here.
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    # we need to check if the user is already logged in or not.
+    def get(self,*args, **kwargs):
+        if self.redirect_authenticated_user:
+            #if the user is authenticated then we will retun 
+            return redirect('tasks')
+        # For all other opther cases 
+        return super(RegisterPage,self).get(*args, **kwargs)
 
 class TaskList(LoginRequiredMixin ,ListView):
     model= Task
